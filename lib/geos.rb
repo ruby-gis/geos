d6f5cfc64357d67fea880c7e01dc3532922f7910
Simon Tokumine <simon@japancentre.com>
2010-01-17 20:19:01 +0000

    updated with failing specs

--- lib/geos.rb
+++ lib/geos.rb
@@ -31,34 +31,3 @@ module GEOS
   attach_function :GEOSWKTWriter_destroy, [:pointer], :void
   attach_function :GEOSWKTWriter_write, [:pointer, :pointer], :string
 end
-
-
-readr = GEOS.GEOSWKTReader_create
-writr = GEOS.GEOSWKTWriter_create
-
-geom = GEOS.GEOSWKTReader_read readr, Point.from_lon_lat(1.523, 2.2345).as_wkt
-wkt = GEOS.GEOSWKTWriter_write writr, geom
-
-puts wkt
-
-
-
-
-###COMPLETE RUBBISH FOLLOWS...
-#puts GEOS.GEOSisValid geom1
-#wkt2 = Point.from_lon_lat(50, 50).as_wkt
-#geom2 = GEOS.GEOSWKTReader_read readr, wkt2
-#dist_ptr = FFI::MemoryPointer.new :double
-#coords = GEOS.GEOSDistance geom1, geom2, dist_ptr
-#puts coords
-#puts dist_ptr.get_double(0)
-##geom = GEOS.GEOSGeomFromWKT(Point.from_lon_lat(1.523, 2.2345).as_wkt)
-#wkt = GEOS.GEOSGeomToWKT(geom)
-#puts wkt
-
-=begin
-/* WKT Writer */
-extern GEOSWKTWriter GEOS_DLL *GEOSWKTWriter_create();
-extern void GEOS_DLL GEOSWKTWriter_destroy(GEOSWKTWriter* writer);
-extern char GEOS_DLL *GEOSWKTWriter_write(GEOSWKTWriter* reader, const GEOSGeometry* g);
-=end
--- spec/geos_spec.rb
+++ spec/geos_spec.rb
@@ -1,11 +1,22 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Geos" do
+  
+  it "should import a geom from WKT" do
+    readr = GEOS.GEOSWKTReader_create
+    geom = GEOS.GEOSWKTReader_read readr, "POINT(1.523 2.2345)"
+    assert false #TODO: find a way to test this    
+  end
 
-  it "should import a geom from WKT"
-
-  it "should export a geom to WKT"
-
+  it "should export a geom to WKT" do
+    readr = GEOS.GEOSWKTReader_create
+    writr = GEOS.GEOSWKTWriter_create
+    
+    geom = GEOS.GEOSWKTReader_read readr, "POINT(1.523 2.2345)"
+    wkt = GEOS.GEOSWKTWriter_write writr, geom
+    assert false #TODO: find a way to test this
+  end
+  
   it "should import a geom from WKB"
 
   it "should export a geom to WKB"
