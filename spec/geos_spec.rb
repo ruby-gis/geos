c5e9166660462c0ff5cc773e406e1f163b2bc967
Marcos Piccinini <x@nofxx.com>
2010-01-17 18:09:27 -0200

    path finding

--- spec/geos_spec.rb
+++ spec/geos_spec.rb
@@ -1,6 +1,15 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Geos" do
+
+  it "should import a geom from WKT"
+
+  it "should export a geom to WKT"
+
+  it "should import a geom from WKB"
+
+  it "should export a geom to WKB"
+
   it "fails" do
     fail "hey buddy, you should probably rename this file and start specing for real"
   end
